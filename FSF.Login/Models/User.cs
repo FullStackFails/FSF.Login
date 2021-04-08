c7ba4f9a8e333a7c5d6e978a30e2d6ab52d4c1da
Patrick Clover <pcloverdevelopment@outlook.com>
2021-04-08 11:56:44 -0600

    Added first name and last name to user registration

---
+++ FSF.Login/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace FSF.Login.Models
+{
+  public class User : IdentityUser
+  {
+    [PersonalData]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    public string LastName { get; set; }
+  }
+}
